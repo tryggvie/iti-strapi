e9af416ea57b84222f063f62189e5100f18d3b4b
Tryggvi Freyr Elinarson <tryggvie@gmail.com>
2025-08-11 01:32:28 +0000

    chore(config): update server url/proxy and Postgres DATABASE_URL

--- config/app.ts
+++ config/app.ts
@@ -1,5 +1,3 @@
 export default ({ env }) => ({
   keys: env.array('APP_KEYS'),
-  name: 'iti-strapi',
-  env: env('NODE_ENV', 'development'),
 });
--- config/server.ts
+++ config/server.ts
@@ -3,7 +3,4 @@ export default ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'http://localhost:1337'),
   proxy: true,
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
 });
