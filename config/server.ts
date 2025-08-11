6f44acc37e7f7beeca7f6f9dbc34b4afc5a83491
Tryggvi Freyr Elinarson <tryggvie@gmail.com>
2025-08-11 02:49:03 +0000

    chore(server): add boot debug + app.keys and port handling

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,17 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
-  proxy: true,
-  app: { keys: env.array('APP_KEYS') },  // <-- add this line
-});
+export default ({ env }) => {
+  console.log('Boot:', {
+    NODE_ENV: env('NODE_ENV'),
+    HOST: env('HOST'),
+    PORT: env('PORT'),
+    PUBLIC_URL: env('PUBLIC_URL'),
+    DB_CLIENT: env('DATABASE_CLIENT'),
+    HAS_DB_URL: !!env('DATABASE_URL'),
+  });
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('PUBLIC_URL', 'http://localhost:1337'),
+    proxy: true,
+    app: { keys: env.array('APP_KEYS') },
+  };
+}
